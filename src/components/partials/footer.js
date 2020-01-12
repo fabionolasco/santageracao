19575a1e62a0179bca5c74402e81930d46d8fbc3
Fabio Nolasco <fabio.nolasco@aig.com>
2020-01-11 21:43:13 -0500

    Improved footer

--- src/components/partials/footer.js
+++ src/components/partials/footer.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import './footer.scss'
 
 export default () => (
-    <footer>
-        Volte sempre
-    </footer>
+    <>
+        <footer className="grid-x">
+            <div className="cell small-12 large-8">
+                <span className="footer-title">Vendas e distribuição:</span><br />
+                Vendas para distribuidores e lojistas:<br />
+                &#8627; Graça Music (21) 3344-5959 / www.gracamusic.com.br<br />
+                Vendas web: www.sg.lojavirtualfc.com.br<br />
+                <br />
+                <span className="footer-title">Contatos por e-mail:</span><br />
+                Agenda: santageracao7@gmail.com ou ivanirantonio.sg@gmail.com<br />
+                Assessoria: santageracao7@gmail.com<br />
+                Financeiro: admfinanceirosg@gmail.com<br />
+                <br />
+                <span className="footer-title">Agenda nos Estados Unidos e demais Nações:</span><br />
+                santageracao7@gmail.com<br />
+                (46) 9912.9620 (Tim) – Ivanir Antônio<br />
+                pracirilo.usa@gmail.com<br />
+                Skype: pr.cirilo<br />&nbsp;&nbsp;&nbsp;
+        </div>
+            <div className="cell small-12 large-4">
+                <span className="footer-title">Contatos:</span><br />
+                (46) 9.9912-9620 (Tim) (WhatsApp)<br />
+                Ivanir Antonio<br />
+                <br />
+                <span className="footer-title">Social Networks:</span><br />
+                <a><i className="fab fa-facebook"></i></a>
+                <a><i className="fab fa-twitter"></i></a>
+                <a><i className="fab fa-instagram"></i></a>
+                <a><i className="fab fa-spotify"></i></a>
+                <a><i className="fab fa-amazon"></i></a>
+            </div>
+        </footer>
+        <div className="grid-x rights">
+            <div className="cell text-center">Santa Geração &copy;</div>
+        </div>
+    </>
 )
